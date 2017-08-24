ad6c7a794df17dc5826d246196572e3e5814d2e1
Andrey Proskurin <andrewproskurin@gmail.com>
2017-08-24 14:16:29 +0300

    Добавил проект

---
+++ Lesson_5_HW/Lesson_5_HW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson_5_HW
+//
+//  Created by Andrey Proskurin on 24.08.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson_5_HW/Lesson_5_HW/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Lesson_5_HW
+//
+//  Created by Andrey Proskurin on 24.08.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
